4046eab93e7d7621e7450cbeb83d0990996f0912
tzero86 <eliasmedina86@gmail.com>
2017-12-08 19:44:31 -0300

    Tutorial Complete
    A Working notes taking app with User Registration/login capabilities. Also has some security in place so users can only remove notes created by themselves.

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-notemanager
+45 Minutes Coding introduction to METEOR.JS
+Link to Video Tutorial: https://youtu.be/9494-2E4riQ
+
+
+Simple Notes Taking app made entirely with Meteor.
+
+Tutorial made by (See Video).
+Coding and Repo made by (tzero86)
--- client/main.js
+++ client/main.js
@@ -1,22 +1,58 @@
 import { Template } from 'meteor/templating';
-import { ReactiveVar } from 'meteor/reactive-var';
+import { Notes } from '../lib/collections.js';
+import { Accounts } from 'meteor/accounts-base';
+
+// accounts config
+Accounts.ui.config({
+  passwordSignupFields: 'USERNAME_ONLY'
+});
 
 import './main.html';
 
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
+Template.body.helpers({
+/*
+  notes:[
+    {text:'My Note 1'},
+    {text:'My Note 2'},
+    {text:'My Note 3'}
+  ]
+  */
+  notes() {
+    return Notes.find({});
+  }
 });
 
-Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
-  },
+Template.add.events({
+  'submit .add-form': function(event){
+    event.preventDefault();
+
+    // Get input value
+    const target = event.target;
+    const text = target.text.value;
+
+    // Insert note into collection
+    Meteor.call('notes.insert', text);
+    /*
+    Notes.insert({
+      text,
+      createdAt: new Date(),
+      owner: Meteor.userId(),
+      username: Meteor.user().username,
+    }); */
+
+    // Clear form
+    target.text.value = "";
+
+    // Close modal
+    $('#addModal').modal('close');
+    return false;
+  }
 });
 
-Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
-  },
+Template.note.events({
+  'click .delete-note': function(){
+    //Notes.remove(this._id);
+    Meteor.call('notes.remove', this);
+    return false;
+  }
 });
